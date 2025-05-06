0aab4ff429b99cd521bb150f6c4178a9cf25c708
Rohan Khanna <rk868@njit.edu>
2025-05-06 01:52:01 -0400

    chore(heroku): create Procfile and fix cors

--- automl-ai-backend/app/main.py
+++ automl-ai-backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"http://localhost:\d+",
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
